ad3538ac50ab41eed8a383a98a1e1f405edfcd6c
Şeymanur Çürükcü <55655002+seymanurcurukcu@users.noreply.github.com>
2024-05-21 01:27:26 +0300

    app.js bug fixed

--- superTraders/db/models/user.js
+++ superTraders/db/models/user.js
@@ -1,103 +1,101 @@
 'use strict';
-const {Model,Sequelize, DataTypes} = require('sequelize');
+const { Model, DataTypes } = require('sequelize');
 const bcrypt = require('bcrypt');
 const sequelize = require('../../config/database');
 const AppError = require('../../utils/appError');
 
-module.exports = sequelize.define('user',{
-  id: {
-    allowNull: false,
-    autoIncrement: true,
-    primaryKey: true,
-    type: DataTypes.INTEGER
-  },
-  firstname: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    validate: {
-        notNull: {
-            msg: 'firstname cannot be null',
-        },
-        notEmpty: {
-            msg: 'firstname cannot be empty',
-        },
-    },
-},
-lastname: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    validate: {
-        notNull: {
-            msg: 'lastname cannot be null',
-        },
-        notEmpty: {
-            msg: 'lastname cannot be empty',
-        },
-    },
-},
-email: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    validate: {
-        notNull: {
-            msg: 'email cannot be null',
-        },
-        notEmpty: {
-            msg: 'email cannot be empty',
-        },
-        isEmail: {
-            msg: 'Invalid email id',
-        },
-    },
-},
-password: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    validate: {
-        notNull: {
-            msg: 'password cannot be null',
-        },
-        notEmpty: {
-            msg: 'password cannot be empty',
-        },
-    },
-},
-confirmPassword: {
-    type: DataTypes.VIRTUAL,
-    set(value) {
-        if (this.password.length < 7) {
-            throw new AppError(
-                'Password length must be grater than 7',
-                400
-            );
+class User extends Model {
+    static associate(models) {
+        User.hasMany(models.Portfolio, { foreignKey: 'UserID' });
+        User.hasMany(models.Trade, { foreignKey: 'UserID' });
+        User.hasMany(models.UserLot, { foreignKey: 'UserID' });
+    }
+    
+    async setPassword(password, confirmPassword) {
+        if (password.length < 7) {
+            throw new AppError('Password length must be greater than 7', 400);
         }
-        if (value === this.password) {
-            const hashPassword = bcrypt.hashSync(value, 10);
-            this.setDataValue('password', hashPassword);
-        } else {
-            throw new AppError(
-                'Password and confirm password must be the same',
-                400
-            );
+        if (password !== confirmPassword) {
+            throw new AppError('Password and confirm password must be the same', 400);
         }
-    },
-},
-createdAt: {
-    allowNull: false,
-    type: DataTypes.DATE,
-},
-updatedAt: {
-    allowNull: false,
-    type: DataTypes.DATE,
-},
-deletedAt: {
-    type: DataTypes.DATE,
-},
-},
+        this.password = await bcrypt.hash(password, 10);
+    }
+}
 
-{
-paranoid: true,
-freezeTableName: true,
-modelName: 'user',
+User.init({
+    id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER
+    },
+    firstname: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            notNull: {
+                msg: 'firstname cannot be null',
+            },
+            notEmpty: {
+                msg: 'firstname cannot be empty',
+            },
+        },
+    },
+    lastname: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            notNull: {
+                msg: 'lastname cannot be null',
+            },
+            notEmpty: {
+                msg: 'lastname cannot be empty',
+            },
+        },
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            notNull: {
+                msg: 'email cannot be null',
+            },
+            notEmpty: {
+                msg: 'email cannot be empty',
+            },
+            isEmail: {
+                msg: 'Invalid email id',
+            },
+        },
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            notNull: {
+                msg: 'password cannot be null',
+            },
+            notEmpty: {
+                msg: 'password cannot be empty',
+            },
+        },
+    },
+    createdAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+    },
+    updatedAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+    },
+    deletedAt: {
+        type: DataTypes.DATE,
+    },
+}, {
+    sequelize,
+    paranoid: true,
+    freezeTableName: true,
+    modelName: 'user',
 });
 
+module.exports = User;
